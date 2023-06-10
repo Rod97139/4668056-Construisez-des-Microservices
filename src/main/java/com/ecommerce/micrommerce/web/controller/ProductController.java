b721922445a98c616d2b19f8acc6dc07f1af51f3
Rod97139 <fayaflame@gmail.com>
2023-06-10 03:52:33 +0200

    part 3: PostMapping

--- src/main/java/com/ecommerce/micrommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/micrommerce/web/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.ecommerce.micrommerce.web.controller;
 
 import com.ecommerce.micrommerce.web.dao.ProductDao;
+import com.ecommerce.micrommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.micrommerce.web.exceptions.ProduitIntrouvableException;
 import com.ecommerce.micrommerce.web.model.Product;
 import io.swagger.annotations.Api;
@@ -59,6 +60,8 @@ public class ProductController {
 
     @PostMapping(value = "/Produits")
     public ResponseEntity<Product> ajouterProduit(@RequestBody @Valid Product product) {
+        
+        //if(product.getPrix()==0) throw new ProduitGratuitException("Le prix de vente ne peut pas être égal à zéro !");
         Product productAdded = productDao.save(product);
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
